2b2abd82e17fb9d3a90a2031356d2dc7b0b5497f
tommieog <tommiekiddy@hotmail.com>
2015-06-15 12:12:16 +0100

    Created new Classes - Original Description to allow minimal editing without affecting codes

---
+++ Nastrology/src/OriginalSignDescription.java
@@ -0,0 +1,16 @@
+
+public class OriginalSignDescription {
+	static String[] signDescription = {"1. Independent, generous, moody, short tempered, optimistic, self-involved, impatient",
+			"2. Stubborn, honest, possession OR materialistic, self-indulging, lazy",
+			"3. Intelligent, thinker, communicative, humourous, multiskilled, indecisive, changeable, adaptable, restless, witty, devious", 
+			"4. Moody, caring, dependable, homely, family-oriented / issues, over-sensitive, insecure, responsive",
+			"5. Confident, melodramatic, generous, loyal, stubborn, vain, pretentious, family-oriented/ family issues",
+			"6. Analytical, observant, fussy, precise, cold, interfering, inflexible, direct, helpful, reliable, not fun",
+			"7. Diplomatic, hospitable, thinker, colourful/likes-drawing/ART, Vain, indecisive, fair, social, intelligent",
+			"8. Resourceful, passionate, loyal, evil, stubborn, determined, obsessive, jealous, intuitive, violent, aggressive, manipulative, ride or die",
+			"9. Exciting, independable, unemotional, adventurous, mutable, philosophical, stubborn, argumentative",
+			"10. Stubborn, resourceful, hardworking, ambition, dictatorial, conceited, emotional, distrusting, reponsible",
+			"11. Witty, communicative, thinker, original, intelligent, aloof, unemotional",
+			"12. Emotional, compassionate, devoted, lazy, imaginative, oversensitiv, self-pitying, escapist"
+			};
+}
---
+++ Nastrology/src/testAltDesc.java
@@ -0,0 +1,12 @@
+
+public class testAltDesc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		signDescription.printOptionMenu();
+		
+
+	}
+
+}
